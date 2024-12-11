41c38a79728f2ff72b1cafb0fb98198a2bd45ffe
Muflih salda maulana <mmuflihsalda4@gmail.com>
2024-12-11 10:42:53 +0700

    first commit

---
+++ view/input_from.py
@@ -0,0 +1,13 @@
+class InputForm:
+    """Kelas untuk merepresentasikan form input data mahasiswa."""
+    
+    def __init__(self):
+        pass
+
+    def input_nama(self):
+        """Menginput nama mahasiswa."""
+        return input("Masukkan nama mahasiswa: ")
+
+    def input_nilai(self):
+        """Menginput nilai mahasiswa."""
+        return input("Masukkan nilai mahasiswa: ")
